e15ad2dd7bee57dff3ee4d432734137ec6770a1a
fedorovse <ingor-ru@mail.ru>
2022-03-01 14:56:42 +0700

    Checkstyle [#4123 #126022]

---
+++ src/main/java/ru/job4j/Info.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Info {
+    @SuppressWarnings("checkstyle:LeftCurly")
+    public static void main(String[] args) {
+        System.out.println("01.03.2022г");
+    }
+}
