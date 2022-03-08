3fc9bce1368a9e68e610f82f3c13a207f61ff5a6
fedorovse <ingor-ru@mail.ru>
2022-03-08 08:59:04 +0700

    5.9. Крест в псевдографике. [#177182 #126085]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -5,7 +5,7 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
         if (number == 1) {
-            prime =false;
+            prime = false;
         } else {
             for (int i = 2; i < number; i++) {
                 if (number % i == 0) {
