2037ca6ff16e169f9bae8e2cd511595390b91e82
fedorovse <ingor-ru@mail.ru>
2022-03-08 17:01:50 +0700

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,5 +6,9 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        System.out.println("Количество элементов в массиве ages: " + ages.length);
+        System.out.println("Количество элементов в массиве surnames: " + surnames.length);
+        System.out.println("Количество элементов в массиве prices: " + prices.length);
     }
 }
